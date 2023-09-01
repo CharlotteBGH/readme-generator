7b99daa88a063a2422328bb857ff9ae8ce5734b0
Charlotte Brazier <charlottebrazier@hotmail.com>
2023-09-01 20:02:27 +0100

    added question prompts for user

--- index.js
+++ index.js
@@ -1,21 +1,66 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Please add a description of your project",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message:
+        "Fantastic. What are the installation instructions for your project?",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "How should users use your project?",
+    },
+    {
+      type: "input",
+      name: "license",
+      message: "What is the license for your project?",
+    },
+    {
+      type: "input",
+      name: "contributing",
+      message:
+        "What are the guidelines for developers to contribute to your project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message:
+        "Please add a short description of which tests users can run on your project",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your email address so people can find out more?",
+    },
+  ]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
